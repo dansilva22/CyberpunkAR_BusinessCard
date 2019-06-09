ccbcd32b17cc4685a98e2e6bb832cc5af830900d
Dan <dansilva_2294@hotmail.com>
2019-06-08 21:25:04 -0400

    no message

--- CyberpunkAR_v3/Assets/Scripts/BuildingGenerator.cs
+++ CyberpunkAR_v3/Assets/Scripts/BuildingGenerator.cs
@@ -30,9 +30,12 @@ public class BuildingGenerator : MonoBehaviour
             //find a random block from the list of blocks
             int rndBlock = Random.Range(0,blocks.Count);
 
-            GameObject curBlock = Instantiate(blocks[rndBlock],spawn.position,Quaternion.identity);
+            GameObject curBlock = Instantiate(blocks[rndBlock],spawn.position,transform.rotation);
             //set parent of the object to this building
             curBlock.transform.SetParent(transform);
+           // curBlock.transform.PositionInParentReference;
+            //
+            curBlock.transform.localScale = transform.localScale;
             //get the spawn point from the new block so that the next block has a position.
             spawn = curBlock.transform.GetChild(0);
             temp.Add(curBlock);
--- CyberpunkAR_v3/Assets/Scripts/GenControl.cs
+++ CyberpunkAR_v3/Assets/Scripts/GenControl.cs
@@ -3,29 +3,119 @@ using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
 
-public class GenControl : DefaultTrackableEventHandler 
+public class GenControl : MonoBehaviour, ITrackableEventHandler 
 {
     public List<GameObject> buildings = new List<GameObject>();
     
     // Start is called before the first frame update
     
 
-    protected override void  OnTrackingFound()
+ /* protected override void  OnTrackingFound()
     {
         Regenerate();
+    }*/
+
+    protected TrackableBehaviour mTrackableBehaviour;
+    protected TrackableBehaviour.Status m_PreviousStatus;
+    protected TrackableBehaviour.Status m_NewStatus;
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
     }
+
+    void OnDestroy()
+    {
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.UnregisterTrackableEventHandler(this);
+    }
+
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        m_PreviousStatus = previousStatus;
+        m_NewStatus = newStatus;
+
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+            OnTrackingFound();
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NO_POSE)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+            OnTrackingLost();
+        }
+        else
+        {
+            // For combo of previousStatus=UNKNOWN + newStatus=UNKNOWN|NOT_FOUND
+            // Vuforia is starting, but tracking has not been lost or found yet
+            // Call OnTrackingLost() to hide the augmentations
+            OnTrackingLost();
+        }
+    }
+
+    void OnTrackingFound()
+    {
+        Debug.Log("Tracking found");
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Enable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = true;
+
+        // Enable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = true;
+
+        // Enable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = true;
+
+        Regenerate();
+    }
+
+    void OnTrackingLost()
+    {
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Disable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = false;
+
+        // Disable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = false;
+
+        // Disable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = false;
+
+        buildings.Clear();
+    }
+
 
 
 
     public void Regenerate()
     {
+        Debug.Log("Regenerating buildings");
         //find all the buildings in the scene 
-        if(buildings.Count == 0){
+       // if(buildings.Count == 0){
             foreach(GameObject building in GameObject.FindGameObjectsWithTag("Building"))
             {
                 buildings.Add(building);
             }
-        }
+      //  }
         
 
         Debug.Log("There are " + buildings.Count + " buildings.");
